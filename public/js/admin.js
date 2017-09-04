2c5579d61616a52d92653951f58b300ac4dd1b3d
Matt Huntington <matt.huntington@gmail.com>
2017-09-04 19:47:11 -0400

    send prompt functionality

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,3 +2,8 @@ const socket = io.connect('/');
 socket.on('users connected', function (data) {
     document.querySelector('dd').innerHTML = data;
 });
+
+document.querySelector('form').addEventListener('submit', (event)=>{
+    event.preventDefault();
+    socket.emit('prompt sent', document.querySelector('[type="text"]').value);
+});
