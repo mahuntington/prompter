fb84f37c52fb91b9d0c204c88376f2c397cc74f9
Matt Huntington <matt.huntington@gmail.com>
2017-09-04 20:05:29 -0400

    creating button

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,9 @@
 const socket = io.connect('/');
+const button = document.querySelector('button')
 socket.on('prompt sent', function (data) {
+    button.removeAttribute("disabled");
     document.querySelector('dd').innerHTML = data;
+});
+button.addEventListener('click', (event)=>{
+    button.setAttribute("disabled",true);
 });
